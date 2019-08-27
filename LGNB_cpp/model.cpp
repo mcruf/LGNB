4c2b6420b528df9eb494330c04e92906fdfed4ea
Marie-Christine Rufener <mruf@win.dtu.dk>
2019-08-27 08:54:19 +0200

    Added R and C++ scripts

---
+++ LGNB_cpp/model.cpp
@@ -0,0 +1,264 @@
+#include <TMB.hpp>
+
+extern "C" void Rf_rmultinom(int, double*, int, int*);
+
+// Multinomial simulator. Move to TMB.
+template<class Type>
+vector<Type> rmultinom(Type size, vector<Type> prob) {
+  int K = prob.size();
+  vector<int> rN(K);
+  int n = (int) asDouble(size);
+  vector<double> p(K);
+  for (int i=0; i<K; i++) p[i] = asDouble(prob(i));
+  p = p / p.sum();
+  Rf_rmultinom(n, p.data(), K, rN.data());
+  vector<Type> ans(K);
+  for (int i=0; i<K; i++) ans[i] = rN[i];
+  return ans;
+}
+
+template<class Type>
+vector<int> sample_replacement(int size, vector<Type> prob) {
+  int K = prob.size();
+  vector<Type> N = rmultinom(Type(size), prob);
+  vector<int> ans(size);
+  int k = 0;
+  for (int i=0; i<K; i++) {
+    for (int j=0; j<asDouble(N(i)); j++) {
+      ans(k) = i; k++;
+    }
+  }
+  return ans;
+}
+
+template<class Type>
+Type objective_function<Type>::operator() ()
+{
+  DATA_SPARSE_MATRIX(Q0);
+  DATA_SPARSE_MATRIX(I);
+  
+  
+  /* Fishery dependent data */
+  DATA_FACTOR(time);      // Length = haulid length
+  DATA_FACTOR(gf);        // Length = haulid length
+  DATA_FACTOR(rowid);     // Length = haulid length; haulid matching the rows of the dataframe (rowid)
+  DATA_VECTOR(response);  // "shorter"
+  DATA_MATRIX(X);         // nrow = response length (Both fixed and random)
+  DATA_VECTOR(offset);
+  //DATA_IVECTOR(fd_indicator); //Length=response length
+  //DATA_IVECTOR(support_area); //Length=nlevels gf
+    
+  /* Random fields */
+  PARAMETER_ARRAY(eta_density);
+  PARAMETER_VECTOR(eta_nugget); // Not used ! perhaps later.
+  
+  /* 5 common fixed effects x number of times */
+  PARAMETER(logdelta);        // Length=2
+  PARAMETER(logscale);         // Dim = c(ntime,2)
+  PARAMETER(logsd_nugget);    // Length = ntime. not used
+  PARAMETER(time_corr);
+  
+  
+  /* Parameters */
+  PARAMETER_VECTOR(beta);         // Fixed effects
+  PARAMETER_VECTOR(beta_r);       // Random effects
+  PARAMETER_VECTOR(beta_r_logsd);
+  DATA_FACTOR(beta_r_fac);
+  PARAMETER_VECTOR(logphi); // NB overdispersion
+  PARAMETER_VECTOR(alpha);
+  
+  /* Stuff for prediction */
+  DATA_INTEGER(doPredict);  // Flag
+  DATA_MATRIX(Xpredict);
+  DATA_FACTOR(Apredict);
+
+  /* Flat prior for 'robustification' */
+  DATA_SCALAR(huge_sd);
+
+  /* Distance in KM between grid points  */
+  DATA_SCALAR(h);
+
+  Type sd_nugget = exp(logsd_nugget);
+  Type ans = 0;
+  using namespace density;
+
+  /* Add *flat* prior to fixed effects */
+  ans -= dnorm(beta, Type(0), huge_sd, true).sum();
+
+  /* Add random effects beta_r */
+  for(int i=0; i<beta_r.size(); i++) {
+    ans -= dnorm(beta_r(i), Type(0), exp(beta_r_logsd(beta_r_fac(i))), true);
+    SIMULATE {
+      beta_r(i) = rnorm(Type(0), exp(beta_r_logsd(beta_r_fac(i))));
+    }
+  }
+  vector<Type> beta_full(X.cols());
+  beta_full << beta, beta_r;
+  if(offset.size() == 0) {
+    offset.resize(X.rows());
+    offset.setZero();
+  }
+
+  
+  /* Optional: Add static field */
+  PARAMETER_VECTOR(eta_static);
+  PARAMETER_VECTOR(logdelta_static);
+  PARAMETER_VECTOR(logscale_static);
+  if(eta_static.size() > 0) {
+    /* Scale parameters for fields */
+    Type scale = exp(logscale_static[0]);
+    /* GMRF: Sparse precision matrix */
+    Eigen::SparseMatrix<Type> Q = Q0 + exp(logdelta_static[0]) * I;
+    GMRF_t<Type> nldens = GMRF(Q);
+    ans += SCALE(nldens, scale)(eta_static);
+    /* Simulate static field */
+    SIMULATE {
+      SCALE(nldens, scale).simulate(eta_static);
+    }
+    int ntimes = NLEVELS(time);
+    for(int i=0; i<ntimes; i++) {
+       eta_density.col(i) -= eta_static;
+    }
+    REPORT(ntimes);
+  }
+
+  /* Time covariance */
+  //N01<Type> nldens_time;
+  Type phi = time_corr / sqrt(1.0 + time_corr*time_corr);
+  AR1_t<N01<Type> > nldens_time = AR1(phi);
+  /* Scale parameters for fields */
+  Type scale = exp(logscale);
+  /* GMRF: Sparse precision matrix */
+  Eigen::SparseMatrix<Type> Q = Q0 + exp(logdelta) * I;
+  GMRF_t<Type> nldens = GMRF(Q);
+  ans += SEPARABLE(SCALE(nldens_time, scale), nldens)(eta_density);
+  /* Simulate space-time random field (and remember to add static) */
+  SIMULATE {
+    SEPARABLE(SCALE(nldens_time, scale), nldens).simulate(eta_density);
+    if(eta_static.size() > 0) {
+      int ntimes = NLEVELS(time);
+      for(int i=0; i<ntimes; i++) {
+        eta_density.col(i) += eta_static;
+      }
+    }
+  }
+  /* Nugget */
+  if(eta_nugget.size() > 0) {
+    ans -= dnorm(eta_nugget, Type(0), sd_nugget, true).sum();
+    SIMULATE {
+      eta_nugget = rnorm(eta_nugget.size(), Type(0), sd_nugget);
+    }
+  }
+
+  /* Include preferential sampling likelihood */
+  DATA_FACTOR(SupportAreaGroup);
+  DATA_IARRAY(SupportAreaMatrix);
+  matrix<Type> probAreaMatrix(NLEVELS(gf),
+                              NLEVELS(time) ); // For simulation only
+  probAreaMatrix.setZero();
+  for (int group = 0; group < NLEVELS(SupportAreaGroup); group++) {
+    vector<int> support_area = SupportAreaMatrix.col(group);
+    vector<Type> logsum (NLEVELS(time));
+    logsum.setZero(); logsum = log(logsum); // logsum = -INFINITY
+    for(int j=0; j<logsum.size();j++){
+      for(int i=0; i<NLEVELS(gf); i++){
+        if(support_area(i)) logsum(j) = logspace_add(logsum(j), alpha[group]*eta_density(i,j));
+      }
+    }
+    // Given 'group' calculate space-time probabilities of trawl positions (used for simulation only)
+    SIMULATE {
+      for(int j=0; j<NLEVELS(time); j++) {
+        for (int i=0; i<NLEVELS(gf); i++) {
+          probAreaMatrix(i, j) = exp( alpha[group] * eta_density(i, j) - logsum( j ) );
+        }
+      }
+    }
+    for(int i=0; i<rowid.size(); i++){
+      if(i==0 || (rowid(i)!=rowid(i-1))){
+        int pos = gf(i);
+        int tim = time(i);
+        // density ~ lambda^alpha
+        if (SupportAreaGroup(rowid(i)) == group) {
+          ans -= alpha[group]*eta_density(pos,tim) - logsum(tim);
+        }
+        SIMULATE {
+          vector<Type> tmp = probAreaMatrix.col(tim).array();
+          gf(i) = sample_replacement(1, tmp)[0];
+          for (int k = i; k<rowid.size() && (rowid(k)==rowid(i)); k++) {
+            gf(k) = gf(i);
+          }
+        }
+      }
+    }
+  }
+  
+  /* Fishery dep data */
+  if(response.size() > 0) {
+    vector<Type> log_mu(response.size());
+    log_mu.setZero(); log_mu = log(log_mu); // log_mu = -INFINITY
+    for(int i=0; i<gf.size(); i++) {
+      if(eta_static.size() == 0) {
+        log_mu(rowid(i)) = logspace_add(log_mu(rowid(i)),
+                                        eta_density(gf[i], time[i]) );
+      } else {
+        log_mu(rowid(i)) = logspace_add(log_mu(rowid(i)),
+                                        eta_density(gf[i], time[i])  + eta_static(gf[i]) );
+      }
+    }
+    log_mu = log_mu + X * beta_full + offset;
+    /* Use numerical robust negative binomial distribution rather than
+       mu = exp(log_mu);
+       var = mu + mu*mu / exp(logphi(0));
+       ans -= dnbinom2(response, mu, var, true).sum(); */
+    vector<Type> log_var_minus_mu = log_mu + log_mu - logphi(0);
+    REPORT(log_mu);            // For debugging
+    REPORT(log_var_minus_mu);  // For debugging
+    ans -= dnbinom_robust(response, log_mu, log_var_minus_mu, true).sum();
+    SIMULATE {
+      vector<Type> mu = exp(log_mu);
+      vector<Type> var = exp(log_var_minus_mu) + mu;
+      response = rnbinom2(mu, var);
+    }
+  }
+
+  SIMULATE {
+    // Simulated random effects
+    REPORT(eta_density);
+    REPORT(eta_static);
+    REPORT(beta_r);
+    // Simulated data
+    REPORT(response);
+    REPORT(gf); // FIXME: Positions not yet sampled
+    // TMB problem: This woodo is needed to keep the factor levels for the simulated gf.
+    Rf_setAttrib( findVar(install("gf"), this -> report),
+                  install("levels"),
+                  Rf_getAttrib( getListElement(this -> data, "gf" ) , install("levels") ) );
+  }
+  
+  if(doPredict){
+    array<Type> logindex = eta_density;
+    // NOTE: eta_density is the *total* field (including the static if present)
+    //if(eta_static.size() > 0) {
+    //  for(int j=0; j<logindex.cols(); j++)
+    //    logindex.col(j) += eta_static;
+    //}
+    // FIXME: Add common fixed effects to predictions (incude covariates available at every grid point).
+    REPORT(logindex);
+    ADREPORT(logindex);
+    
+    /* Note: One can use ADREPORT rather than REPORT to get uncertainties but it may require too much memory */
+    if(isDouble<Type>::value) {
+      Type avgVariance = nldens.variance().mean() * pow(scale, 2.); // This represents the first term in eq.3 in Kristensen et al. (2013)
+      Type delta = exp(logdelta);
+      // Spatial decorrelation distance (corr=exp(-1)) - to compute a plot of spatial correlation vs. distance (km)
+      Type H = h / log(1 + (delta/2) + sqrt(delta + (delta*delta/4)));
+      Type SpatialSD = sqrt(avgVariance); // Convert to standard deviation
+      Type ForecastSD = sqrt(1. - phi*phi) * SpatialSD; // High time corr ==> Better ability to forcast
+      REPORT(H);
+      REPORT(SpatialSD);
+      REPORT(ForecastSD);
+    }
+  }
+
+  return ans;
+}
---
+++ R/utilities.R
@@ -0,0 +1,390 @@
+#=====================
+# Some extra functions
+#=====================
+
+
+# These functions will be used along the LGCP model
+
+
+
+# Function to read all packages in a single line
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+mLoad <- function(...) {
+  sapply(sapply(match.call(), as.character)[-1], require, character.only = TRUE)
+}
+
+
+
+# Function to extract cohorts and create new variable response variable based on that (Response)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Base function to ensure that both datasets have the same timelevels
+extractCohortLevels <- function(d1, d2 = NULL, yearclass=2005, quarterclass=1) {
+  firstYear1 <- min(as.numeric(levels(d1$Year)))
+  firstYear2 <- min(as.numeric(levels(d2$Year)))
+  firstYear <- min(firstYear1, firstYear2)
+  lastYear1 <- max(as.numeric(levels(d1$Year)))
+  lastYear2 <- max(as.numeric(levels(d2$Year)))
+  lastYear <- max(lastYear1, lastYear2)
+  numYears <- lastYear - firstYear + 1
+  #ny <- 1:numYears # to consider only from age_1 group onwards
+  ny <- (1:numYears)-1 # to include age_0 group
+  age0 <- (paste("Age", ny, sep="_"))
+  #year0 <- as.character(yearclass + ny - 1) # to consider only from age_1 group onwards
+  year0 <- as.character(yearclass + ny) # to include age_0 group
+  Quarter <- as.character(rep(1:4, length(age0)))
+  Age <- rep(age0,each=4)
+  Year <- rep(year0,each=4)
+  df <- data.frame(Year,Age,Quarter,stringsAsFactors=FALSE)
+  if (quarterclass > 1) {
+    rem <- (1:(quarterclass-1))
+    df <- df[-rem,]
+  }
+  df$levels <- local(paste("Yearclass_", yearclass, ":", Year,":",Quarter,":", Age, sep=""), df)
+  df
+}
+
+
+
+# Base function to ensure that both datasets have the same timelevels, and which should be specific to each data type
+extractCohortLevels2 <- function(d1, d2 = NULL, yearclass=2005, quarterclass=1) {
+  firstYear1 <- min(as.numeric(levels(d1$Year)))
+  firstYear2 <- min(as.numeric(levels(d2$Year)))
+  firstYear <- min(firstYear1, firstYear2)
+  lastYear1 <- max(as.numeric(levels(d1$Year)))
+  lastYear2 <- max(as.numeric(levels(d2$Year)))
+  lastYear <- max(lastYear1, lastYear2)
+  numYears <- lastYear - firstYear + 1
+  ny <- 1:numYears
+  age0 <- (paste("age", ny, sep="_"))
+  year0 <- as.character(yearclass + ny - 1)
+  datType1 <- rep(levels(d1$Data),length(age0))
+  datType2 <- rep(levels(d2$Data),length(age0))
+  datType  <- rep(c(datType1,datType2), each=4)
+  quarter <- as.character(rep(1:4, length(age0), len=8*length(age0)))
+  age <- rep(age0,each=4,times=2)
+  year <- rep(year0,each=4,times=2)
+  df <- data.frame(year,age,quarter,datType,stringsAsFactors=FALSE)
+  if (quarterclass > 1) {
+    rem <- (1:(quarterclass-1))
+    df <- df[-rem,]
+  }
+  df$levels <- local(paste("Yearclass_", yearclass, ":", year,":",quarter,":", age,"_", datType, sep=""), df)
+  df
+}
+
+
+# Function to extract cohort on a yearly basis
+extract_cohort_year <- function(d, yearclass=2005) {
+  ##yearclass <- 2005 #Year when data starts
+  n <- 1:nlevels(d$Year) #no. of Year levels in the dataset - column name should be the same as in the dataset
+  #n<-1:15
+  age <- paste("age", n, sep="_")
+  year <- as.character(yearclass + n - 1)
+  dnew <- NULL
+  for(i in n) {
+    tmp <- subset(d, Year==year[i])
+    tmp$numberTotal_observer <- tmp[[age[i]]]
+    dnew <- rbind(dnew, tmp)
+  }
+  levels(dnew$Year) <- paste("Yearclass_", yearclass, ":", year,":", age, sep="")
+  dnew
+}
+
+
+
+# Function to extract cohort on a quarter basis within each year
+extract_cohort_quarter <- function(d, df) {
+    n <- nrow(df)
+    dnew <- NULL
+    for(i in 1:n) {
+        tmp <- subset(d, Year==df$Year[i] & Quarter==df$Quarter[i])
+        if (nrow(tmp) > 0) {
+          tmp$Response <- tmp[[df$Age[i]]]
+          tmp$year <- df$levels[i]
+          dnew <- rbind(dnew, tmp)
+        }
+    }
+    dnew$Cohort <- factor(dnew$year, levels=df$levels)
+    dnew$year <- NULL
+    dnew
+}
+
+##df <- extractCohortLevels(d, d)
+##ec <- extract_cohort_quarter(d, df)
+
+
+
+# Function for binding commercial and survey datasets into one single dataset
+mybind <- function(x, y) {
+  i <- intersect(names(x), names(y))
+  ix <- setdiff(names(x), names(y))
+  iy <- setdiff(names(y), names(x))
+  z <- rbind(x[i], y[i])
+  xna <- data.frame(lapply(iy, function(...)rep(NA, nrow(x))))
+  names(xna) <- iy
+  yna <- data.frame(lapply(ix, function(...)rep(NA, nrow(y))))
+  names(yna) <- ix
+  zx <- rbind(x[ix], yna)
+  zy <- rbind(xna, y[iy])
+  cbind(z, zx, zy)
+}
+
+
+
+
+# Constructing grid based on existing shapefile
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Use this function if using the contracted versions of the DK shapefile;
+gridConstruct2 <- function(d, km,scale,nearestObs){
+  ## Grid
+  gr <- gridConstruct::gridConstruct(d=d,km=km,scale=scale,nearestObs=nearestObs,filter=FALSE)
+  ## Read shape file data
+  shape <- readOGR(".", "CDK2_cutted") #Importing contracted DK shapefile with width=-0.010
+  proj4string(shape) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+  map <- spTransform(shape,CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
+  gr2 <- as.data.frame(gr)
+  coordinates(gr2) <- ~lon + lat
+  proj4string(gr2) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+  xtra <- over(gr2, map)
+  gr3 <- gr[is.na(xtra$id),]
+  cc <- connectedComponents(gr3)
+  gr3 <- gr3[cc[[which.max(sapply(cc,length))]],]
+  while(any(rowSums(attr(gr3,"pattern")) <=2 ) ){
+    gr3 <- gr3[rowSums(attr(gr3,"pattern"))>2,]
+  }
+  ##plot(map,add=TRUE)
+  f <- function(i){
+    x <- map@polygons[[i]]
+    do.call("rbind",lapply(x@Polygons,function(x)rbind(x@coords,NA)))
+  }
+  qw <- do.call("rbind",lapply(1:length(map@polygons),f))
+  plot(gr3)
+  #points(qw[,1],qw[,2],type="l")
+  polygon(qw[,1],qw[,2],col="grey70")
+  ##gr2 <- spTransform(gr2,CRS(proj4))
+  ##xtra <- over(gr2, shape)
+  ##image(gr,xtra$Lower_,map=NULL)
+  #gr <<- gr3
+  map.pol <<- qw
+  return(gr3)
+  
+}
+
+
+
+
+# Function to filterout unwanted grid cells
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This is a function applied after having constructed de grid.
+# It is used to reduce the ammount of gridcells, and hence keep the grid only for the area of interest.
+# In Kasper's original function, one can fitler out gridcells based on the ICES squares. To do so, ICESsquares are assigned
+# based on the data points. Because we want to extend the grid a bit further to the actual ICESsquares that are based on the datapoints,
+# I did a quick and dirty manual adaptation, where additional ICESsquares were added.
+
+Kattegat <- c("44G0","44G1","43G0","43G1","43G2","42G0","42G1","42G2","41G0","41G1","41G2") #subdivision 21
+Belt <- c("40F9","40G0","40G1","39F9","39G0","39G1","38F9","38G0","38G1","37G0","37G1") #subdivision 22
+Sound <- c("40G2") #subdivision 23
+Baltic_Sea <- c("39G2","39G3","39G4","38G2","38G3","38G4","37G2","37G3","37G4") #subdivision 24
+#Extrasquares <- c("36G4","36G3","40G4","37G5","38G5","39G5","40G5","41G5","41G4","45G1","45G0")
+Extrasquares <- c("36G4","36G3","40G4","37G5","38G5","39G5","40G5","41G5","41G4","40G3")
+
+studyarea <- c(Kattegat,Belt,Sound,Baltic_Sea,Extrasquares)
+
+
+##' @param icesSquare Remove grid points outside ICES squares in the data?
+##' @param nearestObs Remove grid points with closest data point greater than \code{nearestObs}.
+##' @param wet Remove grid points on land.
+##' @param wetEdges Alternative: Keep edges passing through water. 
+##' @param ordertol Require at least \code{ordertol} neighbors to every grid point.
+##' @param connected Keep only largest connected component.
+##' @return Filtered grid object
+##' @rdname grid
+gridFilter <- function(grid,data,
+                       icesSquare=FALSE,
+                       nearestObs=Inf,
+                       wet=FALSE,
+                       connected=FALSE,
+                       ordertol=0,
+                       wetEdges=FALSE,
+                       ...){
+  keep <- rep(TRUE,nrow(grid))
+  ## icesSquare filtering
+  if(icesSquare){
+    require(DATRAS)
+    #sq <- unique(DATRAS::icesSquare(data)) #Kasper's original code
+    sq <- studyarea #My own contribution
+    ##pol <- icesSquare2coord(sq,format="polygons")
+    ##keep <- keep & !is.na(cut(grid,pol))
+    sqg <- DATRAS::icesSquare(grid)
+    keep <- keep & (sqg %in% sq)
+  }
+  ## nearest observation filtering
+  if(is.finite(nearestObs)){
+    ## i <- gridLocate(data,grid)
+    ## d <- dist.km(grid,data[i,],outer=FALSE)
+    ## keep <- keep & (d<=nearestObs)
+    i <- gridLocate(data,grid[keep,])
+    d <- dist.km(grid[keep,],data[i,],outer=FALSE)
+    keep[keep] <- (d<=nearestObs)
+  }
+  ## map filtering
+  if(wet){
+    ##keep <- keep & is.na(cut(grid))
+    keep[keep] <- is.na(cut(grid[keep,]))
+  }
+  if(wetEdges){
+    ## Reduce pattern matrix first
+    Q <- attr(grid,"pattern")
+    Q[!keep,] <- 0
+    Q[,!keep] <- 0
+    attr(grid,"pattern") <- Q
+    ## Now "wetEdgeFilter" has less work
+    grid <- wetEdgeFilter(grid,...)
+  }
+  grid <- grid[keep,]
+  ## Remove gridpoints with too few neighbors
+  if(ordertol>0){
+    rem <- function(i0) {
+      p0 <- attr(grid, "pattern")[i0, i0]
+      cs <- colSums(p0) - 1
+      keep <- cs > ordertol
+      i0 <- i0[keep]
+      if (any(!keep)) 
+        i0 <- rem(i0)
+      i0
+    }
+    i0 <- rem(seq(length=nrow(grid)))
+    grid <- grid[i0,]
+  }
+  ## Keep largest connected component
+  if(connected){
+    c <- connectedComponents(grid)
+    if(length(c)>1){
+      c <- c[[which.max(sapply(c,length))]]
+      grid <- grid[c,]
+    }
+  }
+  grid
+}
+
+
+
+
+# Function to calculate distance between two points
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+distance <- function (lon, lat, lonRef, latRef)
+{
+  pd <- pi/180
+  a1 <- sin(((latRef - lat) * pd)/2)
+  a2 <- cos(lat * pd)
+  a3 <- cos(latRef * pd)
+  a4 <- sin(((lonRef - lon) * pd)/2)
+  a <- a1 * a1 + a2 * a3 * a4 * a4
+  c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+  return(6371 * c)
+}
+
+
+# Function to locate points on nearest grid vertex
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+gridLocateBin2 <- function (grid, points) 
+{
+  recursiveLocate <- function(x, y) {
+    if (nrow(x) == 0 | nrow(y) == 0) 
+      return(NULL)
+    if (nrow(x) == 1) 
+      return(rep(x$key, nrow(y)))
+    x[1:2] <- x[2:1]
+    y[1:2] <- y[2:1]
+    med <- median(x[, 1])
+    i <- factor(x[, 1] < med, levels = c(TRUE, FALSE))
+    spx <- split(x, i)
+    j <- factor(y[, 1] < med, levels = c(TRUE, FALSE))
+    spy <- split(y, j)
+    rm(x, y)
+    ans <- mapply(recursiveLocate, spx, spy, SIMPLIFY = FALSE)
+    if (any(sapply(ans, length) == 0)) 
+      return(unlist(ans))
+    else return(unsplit(ans, j))
+  }
+  grid <- as.data.frame(grid[c("lon", "lat")])
+  points <- as.data.frame(points[c("lon", "lat")])
+  grid$key <- 1:nrow(grid)
+  recursiveLocate(grid, points)
+}
+
+
+# Modified gridFactor function from Kasper's original function
+gridFac <- function (data, grid, ...) {
+            #data <- data[c("lon", "lat")] #Kasper's code
+            data <- data[c("lonMean", "latMean")]
+            #ch <- paste(data$lon, data$lat)
+            ch <- paste(data$lonMean, data$latMean)
+            nd <- which(!duplicated(ch))
+            fac <- unclass(factor(ch, levels = ch[nd]))
+            i <- gridLocate(grid, data[nd, ])
+            fac <- factor(i[fac], 1:nrow(grid))
+            levelAttrib(fac)$grid <- grid
+            class(fac) <- c("gridFactor", class(fac))
+            return(fac)
+}
+
+
+#><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
+
+# Likelihood ratio test (Kasper)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# calculate no. of parameters (param. of main model output + param. fixed effect + param. random eff.)
+# numParms <- function(env0) {
+#   length(env0$obj$par) + sum(env0$obj$env$profile)
+# }
+# 
+# 
+# LRtest <- function(...){
+#   ##suc <- is.null(main)  #Succesive testing?
+#   args <- list(...)
+#   if(is.null(names(args)))names(args) <- sapply(substitute(f(...)),deparse)[-1]
+#   nparms <- sapply(args,numParms)
+#   lik <- sapply(args,function(x)(x$fit$objective))  
+#   casenames <- sub("env","Model",names(args))
+#   suc <- TRUE
+#   if(suc)minus2logQ <- c(NA,2*diff(lik)) else minus2logQ <- 2*(lik-main$fit$objective)
+#   if(suc)df <- c(NA, -diff(nparms)) else df <- length(main$par)-nparms
+#   p <- 1-pchisq(minus2logQ,df=df)
+#   m <- cbind(lik,minus2logQ,nparms,df,p)
+#   rownames(m) <- casenames
+#   ##if(latex)colnames(m)[1:2] <- c('$-\\log L$','$-2\\log Q$')
+#   colnames(m)[1:2] <- c('-log L','-2log Q')
+#   m
+# }
+
+LRT <- function(combined,commercial,survey){
+  #Number of parameters
+  nPar_com <- length(commercial$fit$par) + length(commercial$parameters$beta)
+  nPar_sur <- length(survey$fit$par) + length(survey$parameters$beta)
+  nPar_both <- length(combined$fit$par) + length(combined$parameters$beta)
+  #Negative log-likelihood
+  nll_com <-  commercial$fit$objective
+  nll_sur <-  survey$fit$objective
+  nll_both <-  combined$fit$objective
+  #Degrees of freedom
+  df <- (nPar_com + nPar_sur) - nPar_both 
+  #Likelihood ratio (statistics)
+  LR <- 2*(nll_both - (nll_com + nll_sur))
+  # Likelihood ratio test 
+  p.val <- pchisq(LR, df = df,lower.tail = F)
+  data.frame(LR,df,p.val)
+}
+
+
+
+## FIXME: Not sure if degrees of freedom are righ - I need to check it!
+
+#><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
+
+
+
