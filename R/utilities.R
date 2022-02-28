652eb20e954baa100c0d8109f7805b63f648685e
Marie-Christine Rufener <mruf@dtu.dk>
2022-02-28 20:26:59 +0300

    Update utilities script - GridConstruct function

--- R/utilities.R
+++ R/utilities.R
@@ -1,10 +1,7 @@
-#=====================
-# Some extra functions
-#=====================
 
+# Some helper functions used along the R scripts
 
-# These functions will be used along the LGNB model
-
+## Last update: February 2022
 
 
 ####################################################################################################
@@ -18,111 +15,6 @@ mLoad <- function(...) {
 }
 
 
-
-# Function to extract cohorts and create new variable response variable based on that (Response)
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# Base function to ensure that both datasets have the same timelevels
-extractCohortLevels <- function(d1, d2 = NULL, yearclass=2005, quarterclass=1) {
-  firstYear1 <- min(as.numeric(levels(d1$Year)))
-  firstYear2 <- min(as.numeric(levels(d2$Year)))
-  firstYear <- min(firstYear1, firstYear2)
-  lastYear1 <- max(as.numeric(levels(d1$Year)))
-  lastYear2 <- max(as.numeric(levels(d2$Year)))
-  lastYear <- max(lastYear1, lastYear2)
-  numYears <- lastYear - firstYear + 1
-  #ny <- 1:numYears # to consider only from age_1 group onwards
-  ny <- (1:numYears)-1 # to include age_0 group
-  age0 <- (paste("Age", ny, sep="_"))
-  #year0 <- as.character(yearclass + ny - 1) # to consider only from age_1 group onwards
-  year0 <- as.character(yearclass + ny) # to include age_0 group
-  Quarter <- as.character(rep(1:4, length(age0)))
-  Age <- rep(age0,each=4)
-  Year <- rep(year0,each=4)
-  df <- data.frame(Year,Age,Quarter,stringsAsFactors=FALSE)
-  if (quarterclass > 1) {
-    rem <- (1:(quarterclass-1))
-    df <- df[-rem,]
-  }
-  df$levels <- local(paste("Yearclass_", yearclass, ":", Year,":",Quarter,":", Age, sep=""), df)
-  df
-}
-
-
-
-# Base function to ensure that both datasets have the same timelevels, and which should be specific to each data type
-extractCohortLevels2 <- function(d1, d2 = NULL, yearclass=2005, quarterclass=1) {
-  firstYear1 <- min(as.numeric(levels(d1$Year)))
-  firstYear2 <- min(as.numeric(levels(d2$Year)))
-  firstYear <- min(firstYear1, firstYear2)
-  lastYear1 <- max(as.numeric(levels(d1$Year)))
-  lastYear2 <- max(as.numeric(levels(d2$Year)))
-  lastYear <- max(lastYear1, lastYear2)
-  numYears <- lastYear - firstYear + 1
-  ny <- 1:numYears
-  age0 <- (paste("age", ny, sep="_"))
-  year0 <- as.character(yearclass + ny - 1)
-  datType1 <- rep(levels(d1$Data),length(age0))
-  datType2 <- rep(levels(d2$Data),length(age0))
-  datType  <- rep(c(datType1,datType2), each=4)
-  quarter <- as.character(rep(1:4, length(age0), len=8*length(age0)))
-  age <- rep(age0,each=4,times=2)
-  year <- rep(year0,each=4,times=2)
-  df <- data.frame(year,age,quarter,datType,stringsAsFactors=FALSE)
-  if (quarterclass > 1) {
-    rem <- (1:(quarterclass-1))
-    df <- df[-rem,]
-  }
-  df$levels <- local(paste("Yearclass_", yearclass, ":", year,":",quarter,":", age,"_", datType, sep=""), df)
-  df
-}
-
-
-####################################################################################################
-
-
-# Function to extract cohort on a yearly basis
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-extract_cohort_year <- function(d, yearclass=2005) {
-  ##yearclass <- 2005 #Year when data starts
-  n <- 1:nlevels(d$Year) #no. of Year levels in the dataset - column name should be the same as in the dataset
-  #n<-1:15
-  age <- paste("age", n, sep="_")
-  year <- as.character(yearclass + n - 1)
-  dnew <- NULL
-  for(i in n) {
-    tmp <- subset(d, Year==year[i])
-    tmp$numberTotal_observer <- tmp[[age[i]]]
-    dnew <- rbind(dnew, tmp)
-  }
-  levels(dnew$Year) <- paste("Yearclass_", yearclass, ":", year,":", age, sep="")
-  dnew
-}
-
-
-#########################################################################################
-
-
-# Function to extract cohort on a quarter basis within each year
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-extract_cohort_quarter <- function(d, df) {
-    n <- nrow(df)
-    dnew <- NULL
-    for(i in 1:n) {
-        tmp <- subset(d, Year==df$Year[i] & Quarter==df$Quarter[i])
-        if (nrow(tmp) > 0) {
-          tmp$Response <- tmp[[df$Age[i]]]
-          tmp$year <- df$levels[i]
-          dnew <- rbind(dnew, tmp)
-        }
-    }
-    dnew$Cohort <- factor(dnew$year, levels=df$levels)
-    dnew$year <- NULL
-    dnew
-}
-
-##df <- extractCohortLevels(d, d)
-##ec <- extract_cohort_quarter(d, df)
 
 ####################################################################################################
 
@@ -146,17 +38,19 @@ mybind <- function(x, y) {
 
 ####################################################################################################
 
+
+
 # Constructing grid based on existing shapefile
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Modified from K. Kristensen's original function from gridConstruct package
 # https://github.com/kaskr/gridConstruct/blob/master/gridConstruct/R/grid.R
 
 # Use this function if using the contracted versions of the DK shapefile;
-GridConstruct2 <- function(d, km,scale,nearestObs){
+GridConstruct <- function(d, km,scale,nearestObs){
   ## Grid
   gr <- gridConstruct::gridConstruct(d=d,km=km,scale=scale,nearestObs=nearestObs,filter=FALSE)
   ## Read shape file data
-  shape <- readOGR(".", "CDK2_cutted") #Importing contracted DK shapefile with width=-0.010
+  shape <- readOGR("~/FishCost/Shapefiles", "CDK2_cutted") #Importing contracted DK shapefile with width=-0.010
   proj4string(shape) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
   map <- spTransform(shape,CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
   gr2 <- as.data.frame(gr)
@@ -187,8 +81,7 @@ GridConstruct2 <- function(d, km,scale,nearestObs){
   
 }
 
-
-
+####################################################################################################
 
 # Function to filterout unwanted grid cells
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -197,10 +90,6 @@ GridConstruct2 <- function(d, km,scale,nearestObs){
 # In Kasper's original function, one can fitler out gridcells based on the ICES squares. To do so, ICESsquares are assigned
 # based on the data points. Because we want to extend the grid a bit further to the actual ICESsquares that are based on the datapoints,
 # I did a quick and dirty manual adaptation, where additional ICESsquares were added.
-
-#Modified from K. Kristensen's original function from gridConstruct package
-# https://github.com/kaskr/gridConstruct/blob/master/gridConstruct/R/grid.R
-
 
 Kattegat <- c("44G0","44G1","43G0","43G1","43G2","42G0","42G1","42G2","41G0","41G1","41G2") #subdivision 21
 Belt <- c("40F9","40G0","40G1","39F9","39G0","39G1","38F9","38G0","38G1","37G0","37G1") #subdivision 22
@@ -339,19 +228,22 @@ gridLocateBin2 <- function (grid, points)
 }
 
 
+
+####################################################################################################
+
 # Modified gridFactor function from Kasper's original function
 gridFac <- function (data, grid, ...) {
-            #data <- data[c("lon", "lat")] #Kasper's code
-            data <- data[c("lonMean", "latMean")]
-            #ch <- paste(data$lon, data$lat)
-            ch <- paste(data$lonMean, data$latMean)
-            nd <- which(!duplicated(ch))
-            fac <- unclass(factor(ch, levels = ch[nd]))
-            i <- gridLocate(grid, data[nd, ])
-            fac <- factor(i[fac], 1:nrow(grid))
-            levelAttrib(fac)$grid <- grid
-            class(fac) <- c("gridFactor", class(fac))
-            return(fac)
+  #data <- data[c("lon", "lat")] #Kasper's code
+  data <- data[c("lonMean", "latMean")]
+  #ch <- paste(data$lon, data$lat)
+  ch <- paste(data$lonMean, data$latMean)
+  nd <- which(!duplicated(ch))
+  fac <- unclass(factor(ch, levels = ch[nd]))
+  i <- gridLocate(grid, data[nd, ])
+  fac <- factor(i[fac], 1:nrow(grid))
+  levelAttrib(fac)$grid <- grid
+  class(fac) <- c("gridFactor", class(fac))
+  return(fac)
 }
 
 
@@ -380,30 +272,6 @@ guess_time_effects_in_beta <- function(data, time_levels) {
 
 
 ####################################################################################################
-
-
-# Whenever you are running a model (e.g., GLM within R basic functions), all your information is
-# stored in matrices. So if you, for example, specify a model as response ~ intercept + covariate A + covariate B, 
-# these information will be stored in a so-called design matrix, where you would have typically two
-# vectors (one for the response, another for the intercept) and one covariate matrix.
-# To do so, you could usually rely on the model.matrix R basic function. However, this function
-# provides some serious limitation when some factor levels of a particular covariate present NAs. 
-# The function normally removes empty factor levels, but when dealing with both datasets together 
-# (combined model), keeping all factor levels is very important to fit the model. For instance, 
-# the métier effect is only present in the commercial data. Thus, when binding both datasets, 
-# the final dataframe (herein called as datatot) will have empty factor levels for the survey 
-# data whenever the métier column is regarded.
-
-# Thus, to circumvent this problem, we modified the model.matrix R function within our
-# own created function (buildModelMatrices). The function provides a user-friendly interface, 
-# where you can specify your model in a similar way as done within a GLM(M)/GAM(M) formulation. ¨
-# The nice aspect of this function is that the fixed effect(s), random effect(s) and offset(s)
-# can be defined as a standard model formula (see section 8 for more details).
-
-# Here it should be noted that the function also ensures that the model matrix recognizes
-# immediately which data type is used as an input (commercial, survey or commercial+survey),
-# and thereby adapting itself according to the data considered.
-
 
 
 # Build Matrices for the model
